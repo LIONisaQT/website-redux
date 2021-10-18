49c840fc12625d19dd96c30af1f407e9372abadb
Ryan Shee <ryan.shee@gmail.com>
2021-10-17 17:47:32 -0700

    Add footer

--- src/Sections/AboutMeSection.js
+++ src/Sections/AboutMeSection.js
@@ -63,6 +63,9 @@ function AboutMeSection({ id }) {
 				<p>E-mail: <a className='PrimaryText Link' href='mailto:ryan@ryanshee.com'>ryan@ryanshee.com</a></p>
 				<p>LinkedIn: <a className='PrimaryText Link' href='https://www.linkedin.com/in/ryanzshee/' target='_blank' rel='noreferrer'>LinkedIn</a></p>
 				<p>GitHub: <a className='PrimaryText Link' href='https://github.com/LIONisaQT' target='_blank' rel='noreferrer'>GitHub</a></p>
+				<div className='Footer'>
+					&copy; {new Date().getFullYear()} Ryan Shee
+				</div>
 			</div>
 		</section>
 	)
