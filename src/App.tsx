ec6b542a1a907914a1bc2cae8be10f5c38c5a186
Ryan Shee <7772995+LIONisaQT@users.noreply.github.com>
2024-12-04 13:54:22 -0800

    Basic intro

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Hi, I'm Ryan.</h1>
-      <h2>I like making stuff.</h2>
+      <div id="main">
+        <h1 className="title">Hi, I'm Ryan.</h1>
+        <h2 className="subtitle">I like making stuff.</h2>
+      </div>
     </>
   );
 }
