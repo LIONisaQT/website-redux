04a51b1f5d60b6181e79a3374306034db19e6cbc
Ryan Shee <7772995+LIONisaQT@users.noreply.github.com>
2024-12-04 23:14:50 -0800

    Add fancy effect to text hover

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,65 @@
+import { useState } from "react";
 import "./App.css";
 import FancyButton from "./assets/components/FancyButton/FancyButton";
 
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const originalTitle = "HEY, I'M RYAN.";
+const originalSubtitle = "I like making stuff.";
+const hoverSubtitle = "I might break stuff.";
+
 // TODO: Fill in links.
 function App() {
+  const [title, setTitle] = useState(originalTitle);
+  const [subtitle, setSubtitle] = useState(originalSubtitle);
+
+  const hackTitle = () => {
+    let text = title;
+    let iterations = 0;
+
+    const iterationMultipier = 2.5; // Multiplies the number of times a letter will cycle through random letters. Letter position * multiplier == number of cycles.
+    const intervalSpeed = 30; // Determines time between intervals. Higher == slower iterations.
+
+    const interval = setInterval(() => {
+      text = text
+        .split("")
+        .map((letter, index) => {
+          if (
+            letter === " " ||
+            letter === "," ||
+            letter === "'" ||
+            letter === "."
+          )
+            return letter;
+
+          if (index < iterations) {
+            return originalTitle[index];
+          }
+
+          return letters[Math.floor(Math.random() * 26)];
+        })
+        .join("");
+
+      if (iterations >= originalTitle.length) clearInterval(interval);
+
+      iterations += 1 / iterationMultipier;
+      setTitle(text);
+    }, intervalSpeed);
+  };
+
   return (
     <>
       <div id="main">
         <section className="header">
-          <h1 className="title">Hi, I'm Ryan.</h1>
-          <h2 className="subtitle">I like making stuff.</h2>
+          <h1 onMouseEnter={hackTitle} className="title">
+            {title}
+          </h1>
+          <h2
+            onMouseEnter={() => setSubtitle(hoverSubtitle)}
+            onMouseLeave={() => setSubtitle(originalSubtitle)}
+            className="subtitle"
+          >
+            {subtitle}
+          </h2>
         </section>
         <section className="buttons">
           <FancyButton text="Resume" link="/" isNewTab={true} />
