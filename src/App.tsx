d64f084cbe8cb4cab0933256186ca486c6c22ce0
Ryan Shee <7772995+LIONisaQT@users.noreply.github.com>
2024-12-10 14:00:27 -0800

    Move hacked title into its own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useEffect, useState } from "react";
 import "./App.scss";
 import FancyButton from "./assets/components/FancyButton/FancyButton";
+import HackedTitle from "./assets/components/HackedTitle/HackedTitle";
 
-const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const originalTitle = "HEY, I'M RYAN.";
 const subtitle = "I like making stuff.";
-const ignoredChars = [" ", ",", "'", "."];
 
 type buttonData = {
   text: string;
@@ -32,48 +29,11 @@ const buttons: buttonData[] = [
 ];
 
 function App() {
-  const [title, setTitle] = useState(originalTitle);
-
-  useEffect(() => {
-    hackTitle(); // Run once on startup.
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const hackTitle = () => {
-    let text = title;
-    let iterations = 0;
-
-    const iterationMultipier = 2.5; // Multiplies the number of times a letter will cycle through random letters. Letter position * multiplier == number of cycles.
-    const intervalSpeed = 30; // Determines time between intervals. Higher == slower iterations.
-
-    const interval = setInterval(() => {
-      text = text
-        .split("")
-        .map((char, index) => {
-          if (ignoredChars.includes(char)) return char;
-
-          if (index < iterations) {
-            return originalTitle[index];
-          }
-
-          return alphabet[Math.floor(Math.random() * 26)];
-        })
-        .join("");
-
-      if (iterations >= originalTitle.length) clearInterval(interval);
-
-      iterations += 1 / iterationMultipier;
-      setTitle(text);
-    }, intervalSpeed);
-  };
-
   return (
     <>
       <div id="main">
         <section className="header">
-          <h1 onMouseEnter={hackTitle} className="title">
-            {title}
-          </h1>
+          <HackedTitle title="Hey, I'm Ryan." />
           <div className="subtitle">
             <h2 className="typewriter">{subtitle}</h2>
           </div>
