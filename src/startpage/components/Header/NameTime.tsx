a326053a55c4019229a0a0c65efe18e103a28922
Ryan Shee <7772995+LIONisaQT@users.noreply.github.com>
2025-03-27 16:36:59 -0700

    Update name/time CSS, set time

--- src/startpage/components/Header/NameTime.tsx
+++ src/startpage/components/Header/NameTime.tsx
@@ -1,16 +1,35 @@
+import { useEffect, useState } from "react";
 import "./NameTime.scss";
 
 function NameTime() {
+  const [time, setTime] = useState<string>();
+
+  const updateTime = () => {
+    const date = new Date();
+    const hour = date.getHours() % 12;
+    const minute = date.getMinutes();
+    setTime(`${hour}:${minute}`);
+  };
+
+  useEffect(() => {
+    updateTime();
+    const timer = setInterval(updateTime, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
     <div className="name-time">
       <section className="greetings">
         <div className="greet">Hello,</div>
         <div className="name">Ryan</div>
       </section>
-      <div className="divider">|</div>
+      <div className="divider" />
       <section className="time">
-        <div className="numbers">12:59</div>
-        <div className="ampm">pm</div>
+        <div className="numbers">{time}</div>
+        <div className="ampm">PM</div>
       </section>
     </div>
   );
