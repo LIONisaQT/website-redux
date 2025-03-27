c485166878fe0a5c97965e398211e9ad544073b7
Ryan Shee <7772995+LIONisaQT@users.noreply.github.com>
2025-03-27 12:01:54 -0700

    No idea what I did last but committing anyway for later

--- src/marvel/components/Card/Card.tsx
+++ src/marvel/components/Card/Card.tsx
@@ -1,12 +1,30 @@
+import "./Card.scss";
 import { Hero } from "../../hero-data";
+import { useState } from "react";
 
 function Card(props: { hero: Hero }) {
+  const [isHover, setHover] = useState(false);
+
   return (
-    <div id="card-main">
-      <h2>{props.hero.name}</h2>
-      <img
-        src={new URL(props.hero.portraits.default, import.meta.url).href}
-      ></img>
+    <div className="card-main">
+      {/* <h2>{props.hero.name}</h2> */}
+      <div
+        className="portrait"
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      >
+        <img
+          className={isHover ? "prestige" : "default"}
+          src={
+            new URL(
+              isHover
+                ? props.hero.portraits.prestige
+                : props.hero.portraits.default,
+              import.meta.url
+            ).href
+          }
+        ></img>
+      </div>
     </div>
   );
 }
