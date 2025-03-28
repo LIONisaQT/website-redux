5c92147177a5567f7408e8d46cfc8bd24c4afa31
Ryan Shee <7772995+LIONisaQT@users.noreply.github.com>
2025-03-28 14:15:29 -0700

    Move default bookmark config location

--- src/startpage/components/Bookmarks/custom-bookmark.interface.ts
+++ src/startpage/components/Bookmarks/custom-bookmark.interface.ts
@@ -7,3 +7,79 @@ export interface Bookmark {
   name: string;
   url: string;
 }
+
+// TODO: Read this and move to localStorage on first launch
+export const defaultBookmarks: BookmarkCollection[] = [
+  {
+    name: "Google",
+    bookmarks: [
+      {
+        name: "ryan@ryanshee.com",
+        url: "https://mail.google.com/mail/u/1/",
+      },
+      {
+        name: "ryan.shee@gmail.com",
+        url: "https://mail.google.com/mail/u/0/",
+      },
+      {
+        name: "Drive",
+        url: "https://drive.google.com",
+      },
+      {
+        name: "Photos",
+        url: "https://photos.google.com/u/0/",
+      },
+    ],
+  },
+  {
+    name: "Entertainment",
+    bookmarks: [
+      {
+        name: "Reddit",
+        url: "https://reddit.com",
+      },
+      {
+        name: "YouTube",
+        url: "https://youtube.com",
+      },
+      {
+        name: "Netflix",
+        url: "https://netflix.com",
+      },
+      {
+        name: "Anime",
+        url: "https://hianime.to/",
+      },
+    ],
+  },
+  {
+    name: "Work ",
+    bookmarks: [
+      {
+        name: "GitHub",
+        url: "https://github.com",
+      },
+      {
+        name: "Grind 75",
+        url: "https://www.techinterviewhandbook.org/grind75/",
+      },
+      {
+        name: "LeetCode",
+        url: "https://leetcode.com/",
+      },
+      {
+        name: "myRAM",
+        url: "https://ramid.ccsf.edu/_layouts/PG/login.aspx?ReturnUrl=%2fsso%2fdefault.aspx",
+      },
+    ],
+  },
+  {
+    name: "Utility",
+    bookmarks: [
+      {
+        name: "Jisho",
+        url: "https://jisho.org",
+      },
+    ],
+  },
+];
