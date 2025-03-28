ab40e59fc12b63fa611812c20bc3f15c8ea38de2
Ryan Shee <7772995+LIONisaQT@users.noreply.github.com>
2025-03-28 14:13:18 -0700

    Add edit button for bookmark section

--- src/startpage/components/Bookmarks/BookmarkSection.tsx
+++ src/startpage/components/Bookmarks/BookmarkSection.tsx
@@ -4,7 +4,10 @@ import "./BookmarkSection.scss";
 function BookmarkSection(collection: BookmarkCollection) {
   return (
     <div className="bookmark-section">
-      <div className="collection-name">{collection.name}</div>
+      <div className="title-area">
+        <div className="collection-name">{collection.name}</div>
+        <button className="edit-bookmark">edit</button>
+      </div>
       <ul>
         {collection.bookmarks.map((bookmark) => (
           <li>
