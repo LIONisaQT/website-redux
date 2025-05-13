abdc7fc07faff4cc07d9d655dcf90baf56eb5ec5
Ryan Shee <7772995+LIONisaQT@users.noreply.github.com>
2025-05-13 14:34:40 -0700

    Port Solitaire (#12)

---
+++ src/solitaire/tableau/TableauPile.tsx
@@ -0,0 +1,37 @@
+import type React from "react";
+import PlayingCard from "../card/PlayingCard";
+import type { Card } from "../logic/card";
+import "./TableauPile.css";
+
+interface TableauPileProps {
+	cards: Card[];
+	onClick: (card: Card, origin: Card[]) => void;
+}
+
+const TableauPile: React.FC<TableauPileProps> = ({ cards, onClick }) => {
+	return (
+		<div className="tableau-pile">
+			{cards.length === 0 && (
+				<div className="empty-pile">
+					<p className="empty-text">X</p>
+				</div>
+			)}
+			{cards.map((card, index) => (
+				<PlayingCard
+					key={`${card.rank}-${card.suit}`}
+					card={{
+						rank: card.rank,
+						suit: card.suit,
+						isFaceDown: card.isFaceDown,
+					}}
+					origin={cards}
+					onClick={onClick}
+					zIndex={index}
+					showPeek={index !== cards.length - 1}
+				/>
+			))}
+		</div>
+	);
+};
+
+export default TableauPile;
--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,20 @@ import { dirname, resolve } from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { fileURLToPath } from "node:url";
+import svgr from "vite-plugin-svgr";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        startpage: resolve(__dirname, "startpage/index.html"),
-      },
-    },
-  },
+	plugins: [svgr(), react()],
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "index.html"),
+				startpage: resolve(__dirname, "startpage/index.html"),
+				solitaire: resolve(__dirname, "solitaire/index.html"),
+			},
+		},
+	},
 });
