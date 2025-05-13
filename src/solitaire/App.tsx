4fb6e7cc8c98c39daa350723a591d025f5614872
Ryan Shee <7772995+LIONisaQT@users.noreply.github.com>
2025-05-13 13:56:16 -0700

    Port sounds and win celebration

--- src/solitaire/App.tsx
+++ src/solitaire/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import { Card } from "./logic/card";
 import TableauPile from "./tableau/TableauPile";
@@ -7,19 +7,39 @@ import { Solitaire } from "./logic/solitaire";
 import Foundations from "./foundations/Foundations";
 import FloatingActionButton from "./fab/FloatingActionButton";
 import { FullScreen, useFullScreenHandle } from "react-full-screen";
+import cardFlip from "./assets/sounds/card1.ogg";
+import cardFan from "./assets/sounds/cardFan2.ogg";
+import cancel from "./assets/sounds/cancel.ogg";
+import win from "./assets/sounds/win.ogg";
+import useSound from "use-sound";
+import Modal from "./assets/modal/Modal";
 
 function App() {
+	const [isDevMode, setDevMode] = useState(true);
+	const [fabClickCount, setFabClickCount] = useState(0);
+
+	useEffect(() => {
+		if (fabClickCount < 10) return;
+		setDevMode(true);
+	}, [fabClickCount]);
+
 	const [game, setGame] = useState<Solitaire>();
 	const [, setTableau] = useState<Card[][]>([]);
 	const [, setStock] = useState<Card[]>([]);
-	const [, setWaste] = useState<Card[]>([]);
 	const [, setFoundations] = useState<Card[][]>([]);
+	const [isWon, setWin] = useState(false);
 
 	const handle = useFullScreenHandle();
 
+	const [playCardFlip] = useSound(cardFlip, { volume: 0.25 });
+	const [playCardFan] = useSound(cardFan);
+	const [playCancel] = useSound(cancel, { volume: 0.25 });
+	const [playWin] = useSound(win);
+
 	useEffect(() => {
 		setGame(new Solitaire());
-	}, []);
+		playCardFan();
+	}, [playCardFan]);
 
 	const cardClicked = (card: Card, origin: Card[]) => {
 		if (!game) return;
@@ -28,44 +48,66 @@ function App() {
 			case "tableau":
 			case "flip":
 				setTableau([...game.tableau]);
+				playCardFlip();
 				break;
 			case "foundation":
 				setFoundations([...game.foundations]);
+				playCardFlip();
 				break;
 			default:
+				playCancel();
 				break;
 		}
+
+		let finishedFoundations = 0;
+		game.foundations.forEach((foundation) => {
+			finishedFoundations += foundation.length === 13 ? 1 : 0;
+		});
+		setWin(finishedFoundations === 4);
 	};
 
 	const stockClicked = () => {
 		if (!game) return;
 
 		game.stockClicked();
+		playCardFlip();
 		setStock([...game.stock]);
 	};
 
-	const wasteClicked = (card: Card, origin: Card[]) => {
+	const reset = useCallback(() => {
 		if (!game) return;
 
-		game.cardClicked(card, origin);
-		setWaste([...game.waste]);
-	};
+		setTableau(game.tableau); // Only need this to trigger a re-render
+		setWin(false);
+		playCardFan();
+	}, [game, playCardFan]);
 
-	const restartClicked = () => {
+	const restartClicked = useCallback(() => {
 		if (!game) return;
 
 		game.restartGame();
 		reset();
-	};
+	}, [game, reset]);
 
-	const reset = () => {
-		if (!game) return;
+	useEffect(() => {
+		if (!isWon) return;
+		playWin();
+	}, [isWon, playWin, restartClicked]);
 
-		setTableau(game.tableau); // Only need this to trigger a re-render
+	const winClicked = () => {
+		setWin(true);
 	};
 
 	return (
 		<FullScreen handle={handle}>
+			{isWon && (
+				<Modal
+					titleText="Congratulations!"
+					bodyText="You win! 🎉"
+					onPrimaryClick={restartClicked}
+					primaryButtonText="Restart game"
+				/>
+			)}
 			<div className="play-area">
 				<div className="top-area">
 					<div className="foundations">
@@ -78,7 +120,7 @@ function App() {
 						<Stock
 							game={game}
 							stockClicked={stockClicked}
-							wasteClicked={wasteClicked}
+							wasteClicked={cardClicked}
 						/>
 					</div>
 				</div>
@@ -92,8 +134,11 @@ function App() {
 					))}
 				</div>
 				<FloatingActionButton
+					onClickCallback={() => setFabClickCount(fabClickCount + 1)}
 					fullScreenClicked={handle.active ? handle.exit : handle.enter}
 					restartClicked={restartClicked}
+					winClicked={winClicked}
+					isDevMode={isDevMode}
 				/>
 			</div>
 		</FullScreen>
--- src/solitaire/fab/FloatingActionButton.tsx
+++ src/solitaire/fab/FloatingActionButton.tsx
@@ -1,21 +1,37 @@
 import type React from "react";
 import "./FloatingActionButton.css";
 import { useState } from "react";
+import generic from "../assets/sounds/generic1.ogg";
+import useSound from "use-sound";
 
 interface FabProps {
+	onClickCallback: () => void;
 	fullScreenClicked: () => void;
 	restartClicked: () => void;
+	winClicked: () => void;
+	isDevMode: boolean;
 }
 
 const FloatingActionButton: React.FC<FabProps> = ({
+	onClickCallback,
 	fullScreenClicked,
 	restartClicked,
+	winClicked,
+	isDevMode,
 }) => {
 	const [isOpen, setOpen] = useState(false);
+	const [playGeneric] = useSound(generic);
 
 	return (
 		<div className="fab">
-			<button className="fab-button" onClick={() => setOpen(!isOpen)}>
+			<button
+				className="fab-button"
+				onClick={() => {
+					setOpen(!isOpen);
+					playGeneric();
+					onClickCallback();
+				}}
+			>
 				{isOpen ? "❌" : "➕"}
 			</button>
 			<ul className={`fab-menu-items ${isOpen ? "visible" : "invisible"}`}>
@@ -25,6 +41,7 @@ const FloatingActionButton: React.FC<FabProps> = ({
 						onClick={() => {
 							setOpen(false);
 							fullScreenClicked();
+							playGeneric();
 						}}
 					>
 						<span>⛶</span>
@@ -36,11 +53,25 @@ const FloatingActionButton: React.FC<FabProps> = ({
 						onClick={() => {
 							setOpen(false);
 							restartClicked();
+							playGeneric();
 						}}
 					>
 						<span>↻</span>
 					</button>
 				</li>
+				{isDevMode && (
+					<li className="menu-item">
+						<button
+							className="menu-item-button"
+							onClick={() => {
+								setOpen(false);
+								winClicked();
+							}}
+						>
+							<span>💻</span>
+						</button>
+					</li>
+				)}
 			</ul>
 		</div>
 	);
