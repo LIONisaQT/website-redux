aeef7eb5a2806d7d708183ba9fa74eee1f0717c2
Ryan Shee <7772995+LIONisaQT@users.noreply.github.com>
2025-08-14 18:49:37 -0700

    Add number-rogue link to home page

--- src/App.tsx
+++ src/App.tsx
@@ -5,57 +5,57 @@ import HackedTitle from "./assets/components/HackedTitle/HackedTitle";
 const subtitle = "I like making stuff.";
 
 type buttonData = {
-  text: string;
-  link: string;
-  isNewTab: boolean;
+	text: string;
+	link: string;
+	isNewTab: boolean;
 };
 
 const buttons: buttonData[] = [
-  {
-    text: "Resume",
-    link: "/shee-ryan_resume.pdf",
-    isNewTab: true,
-  },
-  {
-    text: "GitHub",
-    link: "https://github.com/LIONisaQT",
-    isNewTab: true,
-  },
-  {
-    text: "LinkedIn",
-    link: "https://www.linkedin.com/in/ryanzshee/",
-    isNewTab: true,
-  },
+	{
+		text: "Resume",
+		link: "/shee-ryan_resume.pdf",
+		isNewTab: true,
+	},
+	{
+		text: "GitHub",
+		link: "https://github.com/LIONisaQT",
+		isNewTab: true,
+	},
+	{
+		text: "LinkedIn",
+		link: "https://www.linkedin.com/in/ryanzshee/",
+		isNewTab: true,
+	},
 ];
 
 function App() {
-  return (
-    <>
-      <div id="main">
-        <section className="header">
-          <HackedTitle title="Hey, I'm Ryan." />
-          <div className="subtitle">
-            <h2 className="typewriter">{subtitle}</h2>
-          </div>
-        </section>
-        <section className="buttons">
-          {buttons.map((button) => (
-            <FancyButton
-              key={button.text}
-              text={button.text}
-              link={button.link}
-              isNewTab={button.isNewTab}
-            />
-          ))}
-        </section>
-        <section>
-          <a className="blurb" href="/startpage/">
-            Check out my startpage!
-          </a>
-        </section>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div id="main">
+				<section className="header">
+					<HackedTitle title="Hey, I'm Ryan." />
+					<div className="subtitle">
+						<h2 className="typewriter">{subtitle}</h2>
+					</div>
+				</section>
+				<section className="buttons">
+					{buttons.map((button) => (
+						<FancyButton
+							key={button.text}
+							text={button.text}
+							link={button.link}
+							isNewTab={button.isNewTab}
+						/>
+					))}
+				</section>
+				<section>
+					<a className="blurb" href="/number-rogue">
+						Play Number Rogue
+					</a>
+				</section>
+			</div>
+		</>
+	);
 }
 
 export default App;
