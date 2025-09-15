721eda303fa99441b0fcdf7e61253319ab9a2a0a
Ryan Shee <7772995+LIONisaQT@users.noreply.github.com>
2025-09-15 12:07:29 -0700

    Update restaurants list, fix CSS

--- food-now/components/cuisine/Cuisine.tsx
+++ food-now/components/cuisine/Cuisine.tsx
@@ -7,39 +7,58 @@ export interface CuisineOption {
 
 const defaultOptions: CuisineOption[] = [
 	{
-		name: "Japanese",
-		value: "japanese",
-		image: "https://upload.wikimedia.org/wikipedia/commons/5/57/Oseti.jpg",
+		name: "African",
+		value: "african_restaurant",
+		image:
+			"https://foodinstitute.com/wp-content/uploads/2024/01/west-african.jpg",
+	},
+	{
+		name: "American",
+		value: "american_restaurant",
+		image:
+			"https://res.cloudinary.com/hz3gmuqw6/image/upload/c_fill,f_auto,q_60,w_750/v1/classpop/679a768f61781",
+	},
+	{
+		name: "Breakfast",
+		value: "breakfast_restaurants",
+		image:
+			"https://www.tastingtable.com/img/gallery/25-breakfast-foods-popular-in-the-us-ranked-worst-to-best/intro-1711974201.jpg",
+	},
+	{
+		name: "Brunch",
+		value: "brunch_restaurants",
+		image:
+			"https://bigflavorstinykitchen.com/wp-content/uploads/2018/07/Brunch-Board-1.jpg",
+	},
+	{
+		name: "Buffet",
+		value: "buffet_restaurant",
+		image:
+			"https://www.liveabout.com/thmb/LdL9qeKjFZelbKQwhoIKzfjr1wE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/cropped-image-of-hand-taking-food-912173760-5be8229146e0fb00268b3863.jpg",
+	},
+	{
+		name: "Cafes",
+		value: "cafe",
+		image:
+			"https://stepoutbuffalo.com/wp-content/uploads/2022/11/Coffee-Stone-Cafe-Photo-courtesy-of-Coffee-Stone-Cafe5.jpg",
 	},
 	{
 		name: "Chinese",
-		value: "chinese",
+		value: "chinese_restaurant",
 		image:
 			"https://townsquare.media/site/704/files/2022/11/attachment-conor-s-articles-88.jpg?w=780&q=75",
 	},
 	{
-		name: "Mexican",
-		value: "mexican",
+		name: "Dessert",
+		value: "dessert_restaurant",
 		image:
-			"https://domesticfits.com/wp-content/uploads/2024/05/mexican-cuisine-more-than-640x427.jpeg",
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Desserts.jpg/330px-Desserts.jpg",
 	},
 	{
-		name: "Korean",
-		value: "korean",
+		name: "Fast Food",
+		value: "fast_food_restaurant",
 		image:
-			"https://images.yummy.ph/yummy/uploads/2022/04/koreanfoodramyunwithtteokbokki.jpg",
-	},
-	{
-		name: "Vietnamese",
-		value: "vietnamese",
-		image:
-			"https://res.cloudinary.com/hz3gmuqw6/image/upload/c_fill,q_30,w_750/f_auto/vietnamese-street-food-phpgn0u5d",
-	},
-	{
-		name: "American",
-		value: "american",
-		image:
-			"https://res.cloudinary.com/hz3gmuqw6/image/upload/c_fill,f_auto,q_60,w_750/v1/classpop/679a768f61781",
+			"https://www.partstown.com/about-us/wp-content/uploads/2023/11/what-is-considered-fast-food.jpg",
 	},
 	{
 		name: "Filipino",
@@ -48,10 +67,75 @@ const defaultOptions: CuisineOption[] = [
 			"https://makeyourasia.com/templates/yootheme/cache/09/3-09a03ff0.jpeg",
 	},
 	{
+		name: "French",
+		value: "french_restaurant",
+		image:
+			"https://res.cloudinary.com/hz3gmuqw6/image/upload/c_fill,q_30,w_750/f_auto/french-food-phpps9Cr4",
+	},
+	{
+		name: "Greek",
+		value: "greek_restaurant",
+		image:
+			"https://bucketlistjourney.net/wp-content/uploads/2016/06/Gyros-RF-2-2.jpg",
+	},
+	{
+		name: "Indian",
+		value: "indian_restaurant",
+		image:
+			"https://hips.hearstapps.com/hmg-prod/images/chicken-tikka-masala1-1663341991.jpg?crop=0.681xw:1.00xh;0.196xw,0&resize=640:*",
+	},
+	{
+		name: "Indonesian",
+		value: "indonesian_restaurant",
+		image:
+			"https://res.cloudinary.com/rainforest-cruises/images/c_fill,g_auto/f_auto,q_auto/v1622207103/Indonesian-Food-Main/Indonesian-Food-Main.jpg",
+	},
+	{
 		name: "Italian",
-		value: "italian",
+		value: "italian_restaurant",
 		image:
 			"https://www.destinavo.com/wp-content/uploads/2020/01/Italian-Food.jpg",
+	},
+	{
+		name: "Japanese",
+		value: "japanese_restaurant",
+		image: "https://upload.wikimedia.org/wikipedia/commons/5/57/Oseti.jpg",
+	},
+	{
+		name: "Korean",
+		value: "korean_restaurant",
+		image:
+			"https://images.yummy.ph/yummy/uploads/2022/04/koreanfoodramyunwithtteokbokki.jpg",
+	},
+	{
+		name: "Mexican",
+		value: "mexican_restaurant",
+		image:
+			"https://domesticfits.com/wp-content/uploads/2024/05/mexican-cuisine-more-than-640x427.jpeg",
+	},
+	{
+		name: "Middle Eastern",
+		value: "middle_eastern_restaurant",
+		image:
+			"https://ik.imagekit.io/munchery/blog/tr:w-768/introduction-to-middle-eastern-home-cooking.jpeg",
+	},
+	{
+		name: "Thai",
+		value: "thai_restaurant",
+		image:
+			"https://ik.imagekit.io/munchery/blog/tr:w-768/the-10-most-iconic-thai-dishes-and-how-to-make-them-at-home.jpeg",
+	},
+	{
+		name: "Vegetarian",
+		value: "vegetarian_restaurant",
+		image:
+			"https://images.everydayhealth.com/images/diet-nutrition/what-is-a-vegan-diet-benefits-food-list-beginners-guide-alt-1440x810.jpg?w=508",
+	},
+	{
+		name: "Vietnamese",
+		value: "vietnamese_restaurant",
+		image:
+			"https://res.cloudinary.com/hz3gmuqw6/image/upload/c_fill,q_30,w_750/f_auto/vietnamese-street-food-phpgn0u5d",
 	},
 ];
 
@@ -67,31 +151,32 @@ export default function Cuisine({
 	maxCuisines,
 }: CuisineProps) {
 	return (
-		<section className="options-container">
+		<>
 			<h2>{`Pick 1-${maxCuisines} options.`}</h2>
-			<div className="options-grid">
-				{defaultOptions.map((option) => {
-					const isSelected = selectedCuisines.includes(option.value);
-
-					return (
-						<div
-							key={option.value}
-							className={`option-card ${isSelected ? "selected" : ""}`}
-							onClick={() => onCuisineClicked(option.value)}
-						>
-							<img
-								src={option.image}
-								alt={option.name}
-								className="option-image"
-							/>
-							{isSelected && <div className="check-overlay">✔</div>}
-							<div className="option-label">
-								<span>{option.name}</span>
+			<section className="options-container">
+				<div className="options-grid">
+					{defaultOptions.map((option) => {
+						const isSelected = selectedCuisines.includes(option.value);
+						return (
+							<div
+								key={option.value}
+								className={`option-card ${isSelected ? "selected" : ""}`}
+								onClick={() => onCuisineClicked(option.value)}
+							>
+								<img
+									src={option.image}
+									alt={option.name}
+									className="option-image"
+								/>
+								{isSelected && <div className="check-overlay">✔</div>}
+								<div className="option-label">
+									<span>{option.name}</span>
+								</div>
 							</div>
-						</div>
-					);
-				})}
-			</div>
-		</section>
+						);
+					})}
+				</div>
+			</section>
+		</>
 	);
 }
