0b680e595c2a9279e256226590563de021f93438
Ryan Shee <7772995+LIONisaQT@users.noreply.github.com>
2025-09-30 16:59:34 -0700

    Create view/delete placeholder functions

--- dragon-boat/App.tsx
+++ dragon-boat/App.tsx
@@ -2,12 +2,25 @@ import "./App.scss";
 import CrewList from "./components/CrewList/CrewList";
 import { sampleCrew } from "./utils/sample-crew";
 import CrewManager from "./components/CrewManager/CrewManager";
+import { Crew } from "./types";
 
 export default function App() {
+	const onViewClicked = (crew: Crew) => {
+		console.log(`View ${crew.name}`);
+	};
+
+	const onDeleteClicked = (crew: Crew) => {
+		console.log(`Delete ${crew.name}`);
+	};
+
 	return (
 		<>
 			<h1 className="title">Dragon Boat Balancer</h1>
-			<CrewList data={[sampleCrew, sampleCrew, sampleCrew]} />
+			<CrewList
+				data={[sampleCrew, sampleCrew, sampleCrew]}
+				onView={onViewClicked}
+				onDelete={onDeleteClicked}
+			/>
 			<CrewManager />
 		</>
 	);
