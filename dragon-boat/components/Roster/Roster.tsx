d60e00207df73f1e92f2614421946ba3c8b1c86a
Ryan Shee <7772995+LIONisaQT@users.noreply.github.com>
2025-10-10 18:44:03 -0700

    Change wording for import button

--- dragon-boat/components/Roster/Roster.tsx
+++ dragon-boat/components/Roster/Roster.tsx
@@ -51,7 +51,7 @@ export default function Roster({
 			</div>
 			<section className="button-group">
 				<button onClick={addNew}>Add new paddler</button>
-				<button>Import roster from...</button>
+				<button>Import from...</button>
 			</section>
 		</section>
 	);
