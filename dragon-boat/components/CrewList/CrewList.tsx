83406dff0ef25689ae37ea1e25ba60ea0242c02d
Ryan Shee <7772995+LIONisaQT@users.noreply.github.com>
2025-10-12 21:16:30 -0700

    Add copy button

--- dragon-boat/components/CrewList/CrewList.tsx
+++ dragon-boat/components/CrewList/CrewList.tsx
@@ -57,6 +57,22 @@ export default function CrewList({
 							<circle cx="12" cy="12" r="3"></circle>
 						</svg>
 					</button>
+					<button onClick={() => onView?.(row.original)} className="view">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							fill="none"
+							stroke="currentColor"
+							stroke-width="2"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						>
+							<rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
+							<path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
+						</svg>
+					</button>
 					<button
 						onClick={() => onDelete?.(row.original.id)}
 						className="delete"
