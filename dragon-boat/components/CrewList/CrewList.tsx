7a14cc3f40747c86b14d827559d93f7b762800ed
Ryan Shee <7772995+LIONisaQT@users.noreply.github.com>
2025-10-14 11:02:04 -0700

    Update crew list sorting arrow

--- dragon-boat/components/CrewList/CrewList.tsx
+++ dragon-boat/components/CrewList/CrewList.tsx
@@ -130,14 +130,46 @@ export default function CrewList({
 									onClick={header.column.getToggleSortingHandler()}
 									className="table-header"
 								>
-									{flexRender(
-										header.column.columnDef.header,
-										header.getContext()
-									)}
-									{{
-										asc: " 🔼",
-										desc: " 🔽",
-									}[header.column.getIsSorted() as string] ?? null}
+									<div className="header-text-container">
+										{flexRender(
+											header.column.columnDef.header,
+											header.getContext()
+										)}
+										{{
+											asc: (
+												<svg
+													xmlns="http://www.w3.org/2000/svg"
+													width="24"
+													height="24"
+													viewBox="0 0 24 24"
+													fill="none"
+													stroke="currentColor"
+													strokeWidth="2"
+													strokeLinecap="round"
+													strokeLinejoin="round"
+												>
+													<line x1="12" y1="19" x2="12" y2="5"></line>
+													<polyline points="5 12 12 5 19 12"></polyline>
+												</svg>
+											),
+											desc: (
+												<svg
+													xmlns="http://www.w3.org/2000/svg"
+													width="24"
+													height="24"
+													viewBox="0 0 24 24"
+													fill="none"
+													stroke="currentColor"
+													strokeWidth="2"
+													strokeLinecap="round"
+													strokeLinejoin="round"
+												>
+													<line x1="12" y1="5" x2="12" y2="19"></line>
+													<polyline points="19 12 12 19 5 12"></polyline>
+												</svg>
+											),
+										}[header.column.getIsSorted() as string] ?? null}
+									</div>
 								</th>
 							))}
 						</tr>
